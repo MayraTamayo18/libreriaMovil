b4d61500a7cef3d90dd8911874694c28f825b06d
maria del mar <mairatp2005@gmail.com>
2024-06-25 17:39:44 -0500

    actualizacion

--- crudLibreria/app/src/main/java/com/example/crudlibreria/guardarLibroFragment.kt
+++ crudLibreria/app/src/main/java/com/example/crudlibreria/guardarLibroFragment.kt
@@ -9,12 +9,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.android.volley.Request
+import com.android.volley.Request.Method
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.JsonRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.crudlibreria.config.config
+import com.example.crudlibreria.models.libro
+import com.google.gson.Gson
 //import com.google.gson.JsonObject //genera error en el put cun¿ando trabajan con json
 import org.json.JSONObject //esta es la que se trabaja para json
 import java.lang.Exception
@@ -35,7 +38,7 @@ class guardarLibroFragment : Fragment() {
     private var param2: String? = null
 
     // definimos las variables del formulario
-    private var id:Int=0 // el entero no se puede quedar nulo
+    private var id:Int=1 // el entero no se puede quedar nulo
     private lateinit var txtTitulo:EditText
     private lateinit var txtAutor:EditText
     private lateinit var txtIsbn:EditText
@@ -43,6 +46,7 @@ class guardarLibroFragment : Fragment() {
     private lateinit var txtNum_ejem_disponible:EditText
     private lateinit var txtNum_ejem_ocupados:EditText
     private lateinit var btnGuardar:Button
+
 
 
     //try-catch intente hacer una peticion si sale un error captura y se muestra un mesaje evitando que se cierre la aplicacion
@@ -53,6 +57,37 @@ class guardarLibroFragment : Fragment() {
     JsonArrayRequest= responde un arreglo de json
 
     * */
+    //metodo encargado de traer la informacion del libro
+    fun consultarLibro(){
+    // solo se debe consultar si el id es diferente a vacio
+     if(id!=0){
+         var request= JsonObjectRequest(
+             Request.Method.GET, //metodo
+             config.urlLibro, //ur
+             null,//datos de la peticion
+             {response->
+             //variable response contiene la respuesta de la api
+             //se convierte en json a un objeto tipo libro
+                 //generamos un objeto de la libreria gson
+               val gson=Gson()
+                 //se convierte
+               val libro: libro =gson.fromJson(response.toString(),libro::class.java)
+               // se modifica el atributo text de los campos con el valor del objeto
+               txtTitulo.setText(response.getString("titulo"))
+               txtAutor.setText(response.getString("autor"))
+               txtIsbn.setText(response.getString("isbn"))
+               txtGenero.setText(response.getString("genero"))
+               txtNum_ejem_disponible.setText(response.getInt("num_ejem_disponible").toString())
+               txtNum_ejem_ocupados.setText(response.getInt("num_ejem_ocupados").toString())
+                 
+             },//cuando la respuesta es correcta
+             {error->Toast.makeText(context,"Error al consultar",Toast.LENGTH_LONG).show() }//cuando es incorrecta
+         )
+     }
+
+
+
+    }
     fun guardarLibro(){
         // esta clase para que cree o actualizar libro
         try {
@@ -93,8 +128,9 @@ class guardarLibroFragment : Fragment() {
                        return parametros
                    }
                }
+
  */
-               val  parametros=JSONObject()
+              val  parametros=JSONObject()
                parametros.put("titulo",txtTitulo.text.toString())
                parametros.put("autor",txtAutor.text.toString())
                parametros.put("isbn",txtIsbn.text.toString())
@@ -102,12 +138,20 @@ class guardarLibroFragment : Fragment() {
                parametros.put("num_ejem_disponible",txtNum_ejem_disponible.text.toString())
                parametros.put("num_ejem_ocupados",txtNum_ejem_ocupados.text.toString())
 
+               /*val  parametros=JSONObject()
+               parametros.put("titulo",txtTitulo.text.toString())
+               parametros.put("nombre_autor",txtAutor.text.toString())
+               parametros.put("isbn",txtIsbn.text.toString())
+               parametros.put("genero",txtGenero.text.toString())
+               parametros.put("num_ejem_disponibles",txtNum_ejem_disponible.text.toString())
+               parametros.put("num_ejem_ocupados",txtNum_ejem_ocupados.text.toString())*/
+
                var request= JsonObjectRequest(
                    Request.Method.POST, //metodo
                    config.urlLibro, //ur
                    parametros,//datos de la peticion
                    {response->Toast.makeText( context,"se guardor correctamente", Toast.LENGTH_SHORT).show() },//cuando la respuesta es correcta
-                   
+
                    {error->Toast.makeText(context, "se genero un error", Toast.LENGTH_LONG).show() }//cuando es incorrecta
                )
 
