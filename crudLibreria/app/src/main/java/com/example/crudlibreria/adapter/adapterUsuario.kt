ae7e3b2ff0c4386f38d9aa209840a0d496f8ebcb
maria del mar <mairatp2005@gmail.com>
2024-07-26 12:43:20 -0500

    se termino de crear algunas entidades de usuario

--- crudLibreria/app/src/main/java/com/example/crudlibreria/adapter/adapterUsuario.kt
+++ crudLibreria/app/src/main/java/com/example/crudlibreria/adapter/adapterUsuario.kt
@@ -1,4 +1,77 @@
 package com.example.crudlibreria.adapter
 
-class adapterUsuario {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.crudlibreria.R
+import org.json.JSONArray
+import org.json.JSONObject
+
+class adapterUsuario
+    (
+    var listUsuario: JSONArray,
+    var context: Context,
+
+    ): RecyclerView.Adapter<adapterUsuario.MyHolder>()
+{
+    /*
+    Se crea la clase Myholder
+     */
+    inner class MyHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        /*
+        Dentro de la clase MyHolder se crea las variables
+        y se asocian los objetos de la vista item
+         */
+        lateinit var lblTitulo: TextView
+        lateinit var lblAutor: TextView
+        lateinit var btnEditar: Button
+        lateinit var  btnEliminar: Button
+
+        init{
+            lblTitulo=itemView.findViewById(R.id.lblTitulo)
+            lblAutor=itemView.findViewById(R.id.lblAutor)
+            btnEditar=itemView.findViewById(R.id.btnEditar)
+            btnEliminar=itemView.findViewById(R.id.btnEliminar)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): adapterUsuario.MyHolder {
+        var itemView= LayoutInflater.from(context).inflate(R.layout.item_libros,parent,false)
+        return MyHolder(itemView)
+    }
+
+    //variable que almacena la función onclick nuevo que puse
+    var onclick:((JSONObject)->Unit)?=null
+    var onclickEliminar:((JSONObject)->Unit)?=null
+
+    override fun onBindViewHolder(holder: adapterUsuario.MyHolder, position: Int) {
+        //obtener el registro
+        val libro=listUsuario.getJSONObject(position)
+        //asignar valores
+        holder.lblTitulo.text=libro.getString("titulo")
+        holder.lblAutor.text=libro.getString("autor")
+
+
+        //se crea la función del onclick nuevo que puse
+        holder.btnEditar.setOnClickListener{
+            onclick?.invoke(listUsuario.getJSONObject(position))
+        }
+
+        holder.btnEliminar.setOnClickListener{
+            onclickEliminar?.invoke(listUsuario.getJSONObject(position))
+        }
+    }
+
+    /*
+    getItemCount: retorna el número de elementos
+    de la lista
+     */
+    override fun getItemCount(): Int {
+        return listUsuario.length()
+    }
+
 }
---
+++ crudLibreria/app/src/main/java/com/example/crudlibreria/listarUsuarioFragment.kt
@@ -0,0 +1,183 @@
+package com.example.crudlibreria
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.example.crudlibreria.adapter.adapterUsuario
+import com.example.crudlibreria.config.config
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [listaUsuarioFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+
+class listarUsuarioFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+
+    private lateinit var View: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        View= inflater.inflate(R.layout.fragment_listar_usuario, container, false)
+        cargar_usuario()
+        var btnAgregar=View.findViewById<FloatingActionButton>(R.id.btnAgregar)
+        btnAgregar.setOnClickListener{AgregarUsuario()}
+        return View
+    }
+
+
+    fun cargar_usuario(){
+        try {
+            var request= JsonArrayRequest(
+                Request.Method.GET,
+                config.urlUsuario,
+                null,
+                {response->
+                    var registro=response
+                    //se crea y asocia una variable con el objeto de la vista
+                    var recycler=View.findViewById<RecyclerView>(R.id.RVUsuario)
+                    recycler.layoutManager= LinearLayoutManager(requireContext())
+                    //se crea el adaptador
+                    var adapterUsuario= adapterUsuario(registro,requireContext())
+
+                    //acción cuando se hace click sobre el item nuevo que puse
+                    adapterUsuario.onclick={
+                        //cambio de fragmanto desde otro nuevo
+                        val bundle= Bundle()
+                        bundle.putInt("id",it.getInt("id"))
+                        val transaction=requireFragmentManager()
+                            .beginTransaction()
+                        var fragmento=guardarUsuarioFragment()
+                        fragmento.arguments=bundle
+                        transaction.replace(
+                            R.id.fragmentContainerView,
+                            fragmento).commit()
+                        transaction.addToBackStack(null)
+                    }
+                    adapterUsuario.onclickEliminar={
+                        // mensaje de que si deseas eliminar
+                        val builder = AlertDialog.Builder(requireContext())
+                        builder.setMessage("Desea eliminar este Registro")
+                            .setPositiveButton("Si") { dialog, id ->
+                                // START THE GAME! eliminar funcion  Llmara la funcion  eliminar()
+                                EliminarUsuario(it.getInt("id"))
+                                // redirije a la vista de listar usuario recargada pero aun me aparece un error
+                                val transaction=requireFragmentManager()
+                                    .beginTransaction()
+                                var fragmento=listarUsuarioFragment()
+                                transaction.replace(
+                                    R.id.fragmentContainerView,
+                                    fragmento).commit()
+                                transaction.addToBackStack(null)
+                            }
+                            .setNegativeButton("No") { dialog, id ->
+                                // User cancelled the dialog.
+                            }
+                        // Create the AlertDialog object and return it.
+                        builder.create()
+                        builder.show()
+                    }
+                    //se asocia el adaptador con el objeto
+                    recycler.adapter=adapterUsuario
+                },
+                { error->
+                    Toast.makeText(context,"Error en la consulta", Toast.LENGTH_LONG).show()
+                }
+            )
+            val queue= Volley.newRequestQueue(context)
+            queue.add(request)
+        }catch (e:Exception){
+
+        }
+
+    }
+// crear la funcion de eliminar usuario que es la misma de cargar usuario con el delete
+
+    fun EliminarUsuario(id: Int){
+        try {
+            /*
+            JsonArrayRequest=arreglo json
+            JsonObjectRequest=Json
+            StringRequest=texto, incluyendo ""
+             */
+            var request= JsonObjectRequest(
+                Request.Method.DELETE,
+                config.urlUsuario+id+"/",
+                null,
+                {response->
+                    cargar_usuario()
+                    Toast.makeText(context,"se elimino correctamente ", Toast.LENGTH_LONG).show()
+                },
+                { error->
+                    Toast.makeText(context,"Error", Toast.LENGTH_LONG).show()
+                }
+            )
+            val queue= Volley.newRequestQueue(context)
+            queue.add(request)
+        }catch (e:Exception){
+
+        }
+
+    }
+    fun AgregarUsuario(){
+        val transaction=requireFragmentManager()
+            .beginTransaction()
+        var fragmento=guardarUsuarioFragment()
+        transaction.replace(
+            R.id.fragmentContainerView,
+            fragmento).commit()
+        transaction.addToBackStack(null)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment listaUsuarioFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            listarUsuarioFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
